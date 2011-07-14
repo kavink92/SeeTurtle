4af164e5f01be16ee4fb701224bd98a8c45afbeb
Kavin Karthik <kavink92@gmail.com>
2011-07-14 18:46:31 +0530

    Intended using Vim

--- js/Silk.js
+++ js/Silk.js
@@ -6,7 +6,7 @@
  */
 
 function test_actions() {
-	
+
 	canvascolor (0,100,0);
 	canvassize(700,500);
 	pencolor(200,0,0);
@@ -15,67 +15,67 @@ function test_actions() {
 
 	direction(45);
 	goy(450);
-	
+
 	execute();
-		}
+}
 /*
-draw on the canvas and is called by the execute function */
+   draw on the canvas and is called by the execute function */
 function draw()
 {
-	
+
 	var ex=getx();
-		var ey=gety();
-		var ea=getangle();
-	
-		
+	var ey=gety();
+	var ea=getangle();
+
+
+	forward(10);
+	if(ex>700)
+	{
+		backward(10);
+		if(ea<Math.PI/2)
+			turnleft(90);
+
+		else turnright(90);
 		forward(10);
-		if(ex>700)
-		{
-			backward(10);
-			if(ea<Math.PI/2)
-			turnleft(90);
-			
-			else turnright(90);
-			forward(10);
-		}
-		
-		if(ex<0)
-		{
-			backward(10);
-			if(ea>Math.PI)
-			turnleft(90);
-			else turnright(90);
-			forward(10);
-		}
-		
-		if(ey<0)
-		{
-			backward(10);
-			if(ea>Math.PI/2)
-			turnleft(90);
-			
-			else turnright(90);
-			forward(10);
-		}
-		if(ey>500)
-		{
-			backward(10);
-			if(ea<(3*Math.PI)/2)
-			turnright(90);
-			else turnleft(90);
-			forward(10);
-		}
-		
 	}
+
+	if(ex<0)
+	{
+		backward(10);
+		if(ea>Math.PI)
+			turnleft(90);
+		else turnright(90);
+		forward(10);
+	}
+
+	if(ey<0)
+	{
+		backward(10);
+		if(ea>Math.PI/2)
+			turnleft(90);
+
+		else turnright(90);
+		forward(10);
+	}
+	if(ey>500)
+	{
+		backward(10);
+		if(ea<(3*Math.PI)/2)
+			turnright(90);
+		else turnleft(90);
+		forward(10);
+	}
+
+}
 /*
-execute is the function which calls the draw function after 10 microsecs 
-and immediately calls itself again, 
-by this trick we bring in a timedelay of 10 microsecs
-*/
+   execute is the function which calls the draw function after 10 microsecs 
+   and immediately calls itself again, 
+   by this trick we bring in a timedelay of 10 microsecs
+ */
 function execute()        
 {		
-		setTimeout("draw()",10);
+	setTimeout("draw()",10);
 
-		setTimeout("execute()",10);
-	
+	setTimeout("execute()",10);
+
 }
