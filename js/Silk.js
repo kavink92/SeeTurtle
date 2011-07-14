d255b31f8382d6ed3e9505ad75f38f051c4e41ef
parvathi devi <parvathi@garimella.(none)>
2011-07-14 18:39:59 +0530

    Bugs fixed

--- js/Silk.js
+++ js/Silk.js
@@ -5,6 +5,9 @@
  * 
  */
 
+var i = 0;
+var ex, ey, ea;
+
 function test_actions() {
 	
 	canvascolor (0,100,0);
@@ -15,67 +18,64 @@ function test_actions() {
 
 	direction(45);
 	goy(450);
-	
-	execute();
-		}
-/*
-draw on the canvas and is called by the execute function */
-function draw()
-{
-	
-	var ex=getx();
-		var ey=gety();
-		var ea=getangle();
-	
-		
-		forward(10);
-		if(ex>700)
-		{
-			backward(10);
-			if(ea<Math.PI/2)
-			turnleft(90);
-			
-			else turnright(90);
-			forward(10);
-		}
-		
-		if(ex<0)
-		{
-			backward(10);
-			if(ea>Math.PI)
-			turnleft(90);
-			else turnright(90);
-			forward(10);
-		}
-		
-		if(ey<0)
-		{
-			backward(10);
-			if(ea>Math.PI/2)
-			turnleft(90);
-			
-			else turnright(90);
-			forward(10);
-		}
-		if(ey>500)
-		{
-			backward(10);
-			if(ea<(3*Math.PI)/2)
-			turnright(90);
-			else turnleft(90);
-			forward(10);
-		}
-		
-	}
-/*
-execute is the function which calls the draw function after 10 microsecs 
-and immediately calls itself again, 
-by this trick we bring in a timedelay of 10 microsecs
-*/
-function execute()        
-{		
-		setTimeout("draw()",10);
 
-		setTimeout("execute()",10);
+	ex=getx();
+	ey=gety();
+	ea=getangle();
 	
+	draw();
 }
+
+/* draw on the canvas */
+function draw() {
+
+	var ex=getx();
+	var ey=gety();
+	var ea=getangle();
+
+
+	forward(10);
+	if(ex>700)
+	{
+		backward(10);
+		if(ea<Math.PI/2)
+			turnleft(90);
+
+		else turnright(90);
+		forward(10);
+	}
+
+	if(ex<0)
+	{
+		backward(10);
+		if(ea>Math.PI)
+			turnleft(90);
+		else turnright(90);
+		forward(10);
+	}
+
+	if(ey<0)
+	{
+		backward(10);
+		if(ea>Math.PI/2)
+			turnleft(90);
+
+		else turnright(90);
+		forward(10);
+	}
+
+	if(ey>500)
+	{
+		backward(10);
+		if(ea<(3*Math.PI)/2)
+			turnright(90);
+		else turnleft(90);
+		forward(10);
+	}
+
+	if (i < 10000) {
+		setTimeout(draw_loop, 1);		
+	}
+
+}
+
