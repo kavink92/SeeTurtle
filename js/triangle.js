10f54f8b2044ffa667a5042477256036d7c83312
Kavin Karthik <kavink92@gmail.com>
2011-08-21 11:24:08 +0530

    shifted render function to a new file, I forgot the other change, can you remind me

--- js/triangle.js
+++ js/triangle.js
@@ -90,18 +90,5 @@
 	}
 
 
-	function render()
-	{
-
-
-		if((function_table.length)>0)
-		{
-			
-			(function_table.pop()).execute_function(); //idea for using each variable for a function is that we can pass the arguments for the function also
-                                                                   //execute function will execute each function in the object using their arguments.
-			
-		}
 	
-
-	}
 
